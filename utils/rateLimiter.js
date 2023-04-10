4122ca91ea8a28f6d531ed36318e78a96d1aa69b
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-04-10 23:51:43 +0300

    feat: rate limiter

--- utils/rateLimiter.js
+++ utils/rateLimiter.js
@@ -1,8 +1,8 @@
-const rateLimit = require('express-rate-limit');
+// const rateLimit = require('express-rate-limit');
 
-module.exports.limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  standardHeaders: true,
-  legacyHeaders: false,
-});
+// module.exports.limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000,
+//   max: 100,
+//   standardHeaders: true,
+//   legacyHeaders: false,
+// });
