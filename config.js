0d879d4574ce0f69e6062143aefb6ff67541bc34
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-04-10 23:00:24 +0300

    fix: config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const { JWT_SECRET = 'JWT_SECRET' } = process.env;
 const { PORT = '3002' } = process.env;
-const { DB_ADDRESS = 'mongodb://localhost:27017/movexpdb' } = process.env;
+const { DB_ADDRESS = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
 const { NODE_ENV } = process.env;
 
 module.exports = {
