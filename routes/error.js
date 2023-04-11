e5ce512eac529eccc29f1fbdb9cc568a50bb8e98
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-04-11 15:55:22 +0300

    fix: error route

--- routes/error.js
+++ routes/error.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const { error } = require('../controllers/error');
+const auth = require('../middlewares/auth');
 
+router.use(auth);
 router.all('/*', error);
 
 module.exports = router;
